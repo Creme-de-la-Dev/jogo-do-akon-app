a0da76d0e48cde82de8a2a42d113fb9999341861
gchan26 <guilhermechan30@gmail.com>
2022-10-04 20:13:06 -0300

    Created HomeScreen header

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
 // React
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 // Expo
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 
 // Navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Screens
-import HomeScreen from './app/screens/HomeScreen';
+import HomeScreen from "./app/screens/HomeScreen";
 
 export default function App() {
-
+  // Stack Navigation
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            animationEnabled: false,
+            headerStyle: {
+              backgroundColor: "#fbd276"
+            }
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -28,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -4,21 +4,34 @@ import React, { useContext } from 'react';
 // React Native Components
 import { View, Text, StyleSheet } from 'react-native';
 
+// Expo
+import { StatusBar } from "expo-status-bar";
+
 
 export default HomeScreen = () => {
 
     return (
-        <View style={styles.container}>
-            <Text>HomeScreen</Text>
+        <View style={{flex: 1}}>
+            <StatusBar style="dark" />
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Seja bem-vindo(a) {"\n"}
+                    ao JOGO.
+                </Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
 
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignSelf: 'center'
-    }
+    header: {
+        marginTop: 30,
+        marginLeft: 30,
+    },
+
+    headerText: {
+        fontSize: 26,
+        fontWeight: 'bold',
+    },
+
 });
