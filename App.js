02cd35674d85c31e6d4cc5354af155e41a6cb063
gchan26 <guilhermechan30@gmail.com>
2022-10-04 14:43:56 -0300

    Created HomeScreen file

--- App.js
+++ App.js
@@ -7,15 +7,20 @@ import { StatusBar } from 'expo-status-bar';
 
 // Navigation
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens
+import HomeScreen from './app/screens/HomeScreen';
 
 export default function App() {
-  return (
 
+  const Stack = createNativeStackNavigator();
+
+  return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+// React
+import React, { useContext } from 'react';
+
+// React Native Components
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default HomeScreen = () => {
+
+    return (
+        <View style={styles.container}>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignSelf: 'center'
+    }
+});
