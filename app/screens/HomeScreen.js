1da1dfc8bbce5d5014cee50d99ae603f3e21afe4
gchan26 <guilhermechan30@gmail.com>
2022-10-05 19:45:28 -0300

    How to play card done

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,53 +1,98 @@
 // React
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
 // React Native Components
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import { Card, CardProps } from 'react-native-ui-lib';
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+// import { Card, CardProps } from "react-native-ui-lib";
+import { Card, Button, Icon } from "@rneui/themed";
 
 // Expo
 import { StatusBar } from "expo-status-bar";
 
 // Images
-const GameplayImage = require("../../assets/gameplay.png"); 
-
+const GameplayImage = require("../../assets/gameplay.png");
 
 export default HomeScreen = () => {
-
-    return (
-        <ScrollView style={{flex: 1}}>
-            <StatusBar style="dark" />
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Seja bem-vindo(a) {"\n"}
-                    ao JOGO.
-                </Text>
-            </View>
-            <View style={styles.mainSection}>
-                <Card
-                    height={120}
-                >
-                <Card.Section imageSource={GameplayImage} imageStyle={{height: '100%'}}/>
-                </Card>
-            </View>
-
-        </ScrollView>
-    );
-}
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      <StatusBar style="dark" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>
+          Seja bem-vindo(a) {"\n"}
+          ao JOGO.
+        </Text>
+      </View>
+      <View style={styles.mainSection}>
+        <Card
+          containerStyle={{
+            borderRadius: 10,
+            borderColor: "#fbd276",
+            borderWidth: 2,
+          }}
+        >
+          <Card.Title style={styles.cardTitle}>Como Jogar?</Card.Title>
+          <Card.Divider />
+          <Card.Image
+            style={{ padding: 0, borderRadius: 10 }}
+            source={GameplayImage}
+          />
+          <Text style={styles.cardText}>
+            Aprenda as regras e vire um "expert" no jogo mais amado do mundo!
+          </Text>
+          <Button
+            icon={
+              <Icon
+                name="school"
+                color="#ffffff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={styles.cardButton}
+            title="Aprender as Regras"
+            titleStyle={{ fontWeight: "bold" }}
+          />
+        </Card>
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
+  header: {
+    paddingTop: 30,
+    paddingLeft: 25,
+  },
 
-    header: {
-        paddingTop: 30,
-        paddingLeft: 30,
-    },
+  headerText: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
 
-    headerText: {
-        fontSize: 28,
-        fontWeight: 'bold',
-    },
+  mainSection: {
+    paddingTop: 30,
+  },
 
-    mainSection: {
-        flex: 1,
-    }
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#000000",
+  },
 
+  cardText: {
+    marginBottom: 15,
+    marginTop: 15,
+    fontWeight: "500",
+    alignSelf: "center",
+  },
+
+  cardButton: {
+    borderRadius: 10,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    padding: 15,
+    backgroundColor: "#2176AE",
+    // backgroundColor: "#57B8FF",
+    // backgroundColor: "#FE6847"
+  }
 });
