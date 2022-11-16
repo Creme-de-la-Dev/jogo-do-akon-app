1d23a33d8c46aa509e0b3d6a5b6dea21da5d52ec
gchan26 <guilhermechan30@gmail.com>
2022-11-16 19:26:11 -0300

    online match page done

--- src/tabs/OnlineMatch.js
+++ src/tabs/OnlineMatch.js
@@ -2,15 +2,24 @@
 import React, { useContext } from "react";
 
 // React Native Components
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import SVG from "react-native-svg";
-import SoonImage from "../../assets/soon.svg";
+
+// Image
+import onlineMode from "../../assets/onlineMode.png";
 
 export default OnlineMatch = () => {
   return (
     <View style={styles.container}>
-      {/* <SoonImage width={48} height={48} /> */}
-      <Text style={{ color: "#000", fontSize: 28, fontWeight: "bold" }}>
+      <Image style={styles.image} source={onlineMode} />
+      <Text
+        style={{
+          color: "#000",
+          fontSize: 28,
+          fontWeight: "bold",
+          alignSelf: "center",
+        }}
+      >
         Em Breve
       </Text>
     </View>
@@ -22,5 +31,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignSelf: "center",
+  },
+
+  image: {
+    width: 350,
+    height: 350,
+    resizeMode: "contain",
   },
 });
