251a7bc08e013e339481c4370e58c2f97edcc79f
gchan26 <guilhermechan30@gmail.com>
2022-12-07 14:54:36 -0300

    Created loading screen

--- app/screens/ClassicGameplay.js
+++ app/screens/ClassicGameplay.js
@@ -2,8 +2,10 @@
 import React, { useState } from "react";
 
 // React Native Components
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Bars } from "react-native-loader";
 
 // Expo
 import { LinearGradient } from "expo-linear-gradient";
@@ -11,13 +13,22 @@ import { StatusBar } from "expo-status-bar";
 
 // Icons
 import MCIcon from "react-native-vector-icons/MaterialCommunityIcons";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default ClassicGameplay = () => {
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
 
-  return (
+  return loading ? (
+    <View>
+    <StatusBar style="dark" />
+      <LinearGradient colors={["#FCC54A", "#FECD5E"]}>
+        <View style={[styles.container, {justifyContent: "center"}]}>
+            <ActivityIndicator size="large" color={"#000"}/>
+        </View>
+      </LinearGradient>
+    </View>
+  ) :
+    (
     <View>
       <StatusBar style="dark" />
       <LinearGradient colors={["#FCC54A", "#FECD5E"]}>
@@ -43,6 +54,11 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     height: "100%",
     padding: 8,
+  },
+
+  loadingContainer: {
+    justifyContent: "center",
+    height: "100%",
   },
 
   header: {
