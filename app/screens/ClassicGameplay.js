d2d6e06f5ee421cfbba264b359cc3c0fa734e867
gchan26 <guilhermechan30@gmail.com>
2022-12-07 11:52:27 -0300

    Created back button

--- app/screens/ClassicGameplay.js
+++ app/screens/ClassicGameplay.js
@@ -1,27 +1,54 @@
 // React
-import React, { useContext } from 'react';
+import React, { useState } from "react";
 
 // React Native Components
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 // Expo
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 
-export default ClassicGameplay = () => {
+// Icons
+import MCIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-    return (
+export default ClassicGameplay = () => {
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
+
+  return (
+    <View>
+      <StatusBar style="dark" />
+      <LinearGradient colors={["#FCC54A", "#FECD5E"]}>
         <View style={styles.container}>
-            <Text>ClassicGameplay</Text>
+          <View style={styles.header}>
+            <TouchableOpacity style={styles.backBtn} onPress={()=>{navigation.goBack();}}>
+              <MCIcon
+                name="arrow-left"
+                color="#ffffff"
+                iconStyle={{ marginRight: 10 }}
+                size={35}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-    );
-}
+      </LinearGradient>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  container: {
+    // justifyContent: "center",
+    height: "100%",
+    padding: 8,
+  },
 
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignSelf: 'center'
-    }
+  header: {
+    width: "10%",
+    height: "10%",
+    alignSelf: "flex-start",
+    justifyContent: "flex-end",
+  },
 });
